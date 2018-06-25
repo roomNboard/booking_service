6029580e3a968a711babb9f96de160c2b58fd9cd
clfhhc <clfhhc@gmail.com>
2018-06-25 10:28:37 -0700

    middle of mongodb setup

---
+++ database/mongoDb/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost/booking_service');
+
+mongoose.Promise = global.Promise;
+
+const db = mongoose.connection;
+db.once('open', (err) => {
+  if (err) console.log('Connection error:', err);
+  console.log('Mongoose Connection is up');
+});
+
+module.exports = db;
