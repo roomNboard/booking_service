54033bdfe347fb23af2303632489e585ddcd6f55
clfhhc <clfhhc@gmail.com>
2018-07-04 12:05:55 -0700

    adding more console log to see issue

--- server/api/routes/booking.js
+++ server/api/routes/booking.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const db = require('../../../database/index');
 const redisClient = require('../../../database/redis/index');
+const preDb = require('../../../database/postgreSQL/db');
 
 const router = express.Router();
 
@@ -48,6 +49,8 @@ router.post('/:id', async (req, res, next) => {
       }
       const { bookings } = result;
       if (validateBooking(bookings, booking)) {
+        console.log(await preDb().quey(`SELECT MAX(the_primary_key) FROM the_table;`));
+        console.log(await preDb().query(`SELECT nextval('the_primary_key_sequence');`));
         await db.insertBookingInfo(booking);
         res.sendStatus(201);
       } else {
